08984a3b02b480fa98ac771f1e672cac28cae409
JendaJeFrajer <Wotko12589@seznam.cz>
2020-06-04 12:49:02 +0200

    :)

---
+++ app/src/main/java/com/example/magicgame/Upgrades.java
@@ -0,0 +1,27 @@
+package com.example.magicgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Upgrades extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
